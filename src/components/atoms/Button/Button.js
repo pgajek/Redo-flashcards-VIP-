adb45edabd366c04da61c0f9d5b5d17902bd4779
Patryk Gajek <pgajek94@gmail.com>
2019-07-26 13:03:57 +0200

    button stories with knobs

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -2,20 +2,20 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 
 const Button = styled.button`
-  background-color: ${({ color }) => color || '#ffd82b'};
+  background-color: ${({ color }) => color || 'hsl(49, 100%, 58%)'};
   width: 220px;
   height: 47px;
   border: none;
   border-radius: 50px;
   font-family: 'Montserrat';
-  font-weight: 600;
+  font-weight: 500;
   text-transform: uppercase;
   cursor: pointer;
   transition: 0.2s;
   ${({ secondary }) =>
     secondary &&
     css`
-      background-color: ${({ color }) => color || '#e6e6e6'};
+      background-color: ${({ color }) => color || 'hsl(0, 0%, 90%)'};
       width: 105px;
       height: 30px;
       font-size: 1rem;
--- src/components/atoms/Button/Button.stories.js
+++ src/components/atoms/Button/Button.stories.js
@@ -3,17 +3,19 @@ import Button from './Button';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, select } from '@storybook/addon-knobs';
 
-const label = 'Colors';
-const options = {
-  primarry: 'hsl(49, 100%, 58%)',
-  secondary: 'hsl(196, 83%, 75%',
-  tertiary: 'hsl(106, 47%, 64%)',
-};
-const defaultValue = 'hsl(49, 100%, 58%)';
-const groupId = 'GROUP-ID1';
-
-const value = select(label, options, defaultValue, groupId);
-
 storiesOf('Button', module)
-  .add('primarry', () => <Button color={value}>Kututut</Button>)
+  .addDecorator(withKnobs)
+  .add('primarry', () => {
+    const label = 'Colors';
+    const options = {
+      primarry: 'hsl(49, 100%, 58%)',
+      secondary: 'hsl(196, 83%, 75%)',
+      tertiary: 'hsl(106, 47%, 64%)',
+    };
+    const defaultValue = 'hsl(49, 100%, 58%)';
+    const groupId = 'GROUP-ID1';
+
+    const value = select(label, options, defaultValue, groupId);
+    return <Button color={value}>Kututut</Button>;
+  })
   .add('secondary', () => <Button secondary>Secondary</Button>);
