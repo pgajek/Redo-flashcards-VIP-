df70ab01289bd6dc0f952b9037df8163d579960e
Patryk Gajek <pgajek94@gmail.com>
2019-07-26 12:56:18 +0200

    storybbok knobs installed, globalstyles for storybook added

--- src/components/atoms/Button/Button.stories.js
+++ src/components/atoms/Button/Button.stories.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import Button from './Button';
 import { storiesOf } from '@storybook/react';
+import { withKnobs, select } from '@storybook/addon-knobs';
 
-storiesOf('Button', module).add('primarry', () => <Button>Kututut</Button>);
+const label = 'Colors';
+const options = {
+  primarry: 'hsl(49, 100%, 58%)',
+  secondary: 'hsl(196, 83%, 75%',
+  tertiary: 'hsl(106, 47%, 64%)',
+};
+const defaultValue = 'hsl(49, 100%, 58%)';
+const groupId = 'GROUP-ID1';
+
+const value = select(label, options, defaultValue, groupId);
+
+storiesOf('Button', module)
+  .add('primarry', () => <Button color={value}>Kututut</Button>)
+  .add('secondary', () => <Button secondary>Secondary</Button>);
