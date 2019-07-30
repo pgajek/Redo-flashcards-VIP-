82f86d12dee4e1b9af1902cb06ddd9cbce157e5c
Patryk Gajek <pgajek94@gmail.com>
2019-07-30 14:28:12 +0200

    first theme

---
+++ src/theme/mainTheme.js
@@ -0,0 +1,27 @@
+const theme = {
+  primary: '',
+  secondary: '',
+  tertiary: '',
+  fontLight: 300,
+  fontMedium: 600,
+  fontBold: 700,
+  darkTheme: {
+    mainDark: 'hsl(199, 53%, 24%)',
+    deepDark: 'hsl(202, 64%, 15%)',
+    medium: 'hsl(184, 50%, 33%)',
+    light: 'hsl(168, 68%, 60%)',
+    light2: 'hsl(180, 55%, 48%)',
+    lightGradient: 'linear-gradient(90deg, hsl(168, 68%, 60%) 0%, hsl(180, 55%, 48%) 100%)',
+    text: 'hsl(0,0,0)',
+  },
+  lightTheme: {
+    mainDark: 'hsl(199, 53%, 24%)',
+    deepDark: 'hsl(202, 64%, 15%)',
+    medium: 'hsl(184, 50%, 33%)',
+    light: 'hsl(168, 68%, 60%)',
+    light2: 'hsl(180, 55%, 48%)',
+    lightGradient: 'linear-gradient(90deg, hsl(168, 68%, 60%) 0%, hsl(180, 55%, 48%) 100%)',
+  },
+};
+
+export default theme;
